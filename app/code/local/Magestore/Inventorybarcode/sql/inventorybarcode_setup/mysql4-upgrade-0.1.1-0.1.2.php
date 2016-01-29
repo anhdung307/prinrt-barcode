72e2c31a0d2024905a1a60a60c851ec18a1a694a
Alan(DoDung) <anhdung.do@trueplus.vn>
2016-01-29 09:05:07 +0700

    upgreat table barcore_template

--- app/code/local/Magestore/Inventorybarcode/sql/inventorybarcode_setup/mysql4-upgrade-0.1.1-0.1.2.php
+++ app/code/local/Magestore/Inventorybarcode/sql/inventorybarcode_setup/mysql4-upgrade-0.1.1-0.1.2.php
@@ -27,7 +27,7 @@ try{
             $write->update($this->getTable('erp_inventory_barcode_template'), $data, $where); 
             
         }  else {
-            $write->insert($this->getTable('erp_inventory_barcode_template'), array('barcode_template_id' => 5, 'barcode_template_name' => '', 'html' => '', 'template' => '', 'status' => 1, 'productname_show' => 1, 'sku_show' => 1, 'price_show' => 1, 'barcode_per_row' => '1', 'page_height' => '17', 'barcode_unit' => 0, 'veltical_distantce' => '0', 'horizontal_distance' => '0', 'page_width' => '76', 'top_margin' => '0', 'left_margin' => '0', 'right_margin' => '0', 'bottom_margin' => '0'));   
+            $write->insert($this->getTable('erp_inventory_barcode_template'), array('barcode_template_id' => 5, 'barcode_template_name' => 'Barcode for jewelry', 'html' => '', 'template' => '', 'status' => 1, 'productname_show' => 1, 'sku_show' => 1, 'price_show' => 1, 'barcode_per_row' => '1', 'page_height' => '17', 'barcode_unit' => 0, 'veltical_distantce' => '0', 'horizontal_distance' => '0', 'page_width' => '76', 'top_margin' => '0', 'left_margin' => '0', 'right_margin' => '0', 'bottom_margin' => '0'));   
         }
         
 
